7fef2d6d69e32688c82021061804757e4db01ff4
dolapo-fad <dolapofadipe95@gmail.com>
2026-01-30 04:41:54 -0700

    first-commit

---
+++ main.tf
@@ -0,0 +1,305 @@
+#VPC COMPONENTS
+
+resource "aws_vpc" "terraform6_vpc" {
+  cidr_block = var.vpc_cidr
+
+  tags = {
+    Name = "terraform6_vpc"
+  }
+}
+
+resource "aws_subnet" "public_subnet1" {
+  vpc_id            = aws_vpc.terraform6_vpc.id
+  cidr_block        = "10.0.0.0/24"
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "public_subnet2" {
+  vpc_id            = aws_vpc.terraform6_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1b"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "private_subnet1" {
+  vpc_id            = aws_vpc.terraform6_vpc.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1a"
+}
+
+resource "aws_subnet" "private_subnet2" {
+  vpc_id            = aws_vpc.terraform6_vpc.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "us-east-1b"
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.terraform6_vpc.id
+
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.terraform6_vpc.id
+  depends_on = [aws_internet_gateway.igw]
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+    }
+}
+
+resource "aws_route_table" "private_rt-1" {
+  vpc_id = aws_vpc.terraform6_vpc.id
+  depends_on = [aws_nat_gateway.natgw]
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.natgw.id
+    }
+}
+
+resource "aws_route_table" "private_rt-2" {
+  vpc_id = aws_vpc.terraform6_vpc.id
+  depends_on = [aws_nat_gateway.natgw2]
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.natgw2.id
+    }
+}
+
+resource "aws_route_table_association" "rta" {
+  subnet_id      = aws_subnet.public_subnet1.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "rtb" {
+  subnet_id      = aws_subnet.public_subnet2.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "rtc" {
+  subnet_id      = aws_subnet.private_subnet1.id
+  route_table_id = aws_route_table.private_rt-1.id
+}
+
+resource "aws_route_table_association" "rtd" {
+  subnet_id      = aws_subnet.private_subnet2.id
+  route_table_id = aws_route_table.private_rt-2.id
+}
+
+resource "aws_eip" "nat_eip" {
+  domain = "vpc"
+}
+
+resource "aws_nat_gateway" "natgw" {
+  subnet_id     = aws_subnet.public_subnet1.id
+  allocation_id = aws_eip.nat_eip.id
+}
+
+resource "aws_eip" "nat_eip2" {
+  domain = "vpc"
+}
+resource "aws_nat_gateway" "natgw2" {
+  allocation_id = aws_eip.nat_eip2.id
+  subnet_id     = aws_subnet.public_subnet2.id
+}
+
+# LOADBALANCER
+
+resource "aws_lb" "app_lb" {
+  name               = "app-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_app.id]
+  subnets            = [
+    aws_subnet.public_subnet1.id,
+    aws_subnet.public_subnet2.id
+  ]
+
+  enable_deletion_protection = false
+
+  tags = {
+    Environment = "development"
+  }
+}
+
+resource "aws_lb_target_group" "app_tg" {
+  name     = "app-tg"
+  port     = 8000
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.terraform6_vpc.id
+  target_type = "instance"
+
+    health_check {
+        enabled             = true
+        healthy_threshold   = 3
+        unhealthy_threshold = 3
+        timeout             = 5
+        interval            = 30
+        path                = "/"
+        matcher             = "200-399"
+        port                = "8000"
+    }
+}
+
+resource "aws_lb_listener" "app_listener" {
+  load_balancer_arn = aws_lb.app_lb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app_tg.arn
+  }
+}
+
+# SECURITY GROUPS
+
+resource "aws_security_group" "bastion_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic for bastion host from my IP"
+  vpc_id      = aws_vpc.terraform6_vpc.id
+
+  ingress {
+    description      = "SSH from my-ip"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["68.146.18.77/32"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "private_instance_ssh" {
+  name        = "allow_ssh_bastion"
+  description = "Allow SSH inbound traffic for private instances"
+  vpc_id      = aws_vpc.terraform6_vpc.id
+
+  ingress {
+    description      = "SSH from bastion host"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    security_groups = [aws_security_group.bastion_ssh.id]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "alb_app" {
+  name        = "alb_app"
+  description = "Allow HTTP inbound traffic for application server"
+  vpc_id      = aws_vpc.terraform6_vpc.id
+
+  ingress {
+    description      = "HTTP from ALB"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]    
+    }
+}
+
+resource "aws_security_group" "allow_app" {
+  name        = "allow_app"
+  description = "Allow HTTP inbound traffic for application server"
+  vpc_id      = aws_vpc.terraform6_vpc.id
+
+  ingress {
+    description      = "HTTP from VPC"
+    from_port        = 8000
+    to_port          = 8000
+    protocol         = "tcp"
+    security_groups = [aws_security_group.alb_app.id]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]    
+    }
+}
+
+
+# INSTANCES
+
+resource "aws_key_pair" "terraform_key" {
+  key_name   = "terraform"
+  public_key = file("/home/dolapo/ssh2/id_rsa.pub")
+}
+
+resource "aws_instance" "bastion_host" {
+  ami                    = "ami-0b6c6ebed2801a5cb" 
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.public_subnet1.id
+  key_name               = aws_key_pair.terraform_key.key_name
+  vpc_security_group_ids = [aws_security_group.bastion_ssh.id]
+
+  tags = {
+    Name = "Bastion Host"
+  }
+}
+
+resource "aws_launch_template" "asg" {
+  name_prefix   = "asg-demo"
+  image_id      = "ami-0b6c6ebed2801a5cb"
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.terraform_key.key_name
+
+  user_data = base64encode(templatefile("${path.module}/userdata.tftpl", {
+    html_content = file("${path.module}/index.html")
+  }))
+
+  network_interfaces {
+    associate_public_ip_address = false
+    security_groups             = [aws_security_group.allow_app.id, aws_security_group.private_instance_ssh.id]
+  }
+
+  tags = {
+    Name = "ASG Instance"
+  }
+
+}
+
+resource "aws_autoscaling_group" "terraform-demo" {
+
+  vpc_zone_identifier = [
+    aws_subnet.private_subnet1.id,
+    aws_subnet.private_subnet2.id
+  ]
+  desired_capacity   = 2
+  max_size           = 4
+  min_size           = 1
+  target_group_arns = [aws_lb_target_group.app_tg.arn]
+
+  health_check_type         = "ELB"
+  health_check_grace_period = 300
+
+  launch_template {
+    id      = aws_launch_template.asg.id
+    version = "$Latest"
+  }
+  
+}
+
